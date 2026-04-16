446ecdddd37ea2877a2bd95dc77a88f6992536b9
MostafaY2sser <mostafayasser33mhy@gmail.com>
2026-04-16 16:24:45 +0200

    befor optimization

--- frontend/src/components/dashboard/ProtectedRoute.jsx
+++ frontend/src/components/dashboard/ProtectedRoute.jsx
@@ -1,6 +1,8 @@
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
+
+  // Check if the user is authenticated by looking for a token in localStorage
   const token = localStorage.getItem("token");
 
   if (!token) {
